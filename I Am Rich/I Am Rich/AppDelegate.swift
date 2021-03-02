c8b3d60bbf07894bfc783547aea59d9f6c2ea6ef
woogi <kkjkkjcom1@gmail.com>
2021-03-02 16:33:55 +0900

    add: I Am Rich first commit

---
+++ I Am Rich/I Am Rich/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  I Am Rich
+//
+//  Created by 강태욱 on 2021/03/02.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 강태욱 on 2021/03/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
