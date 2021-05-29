ace8c1824a34b6280d889ed7e128c4daf1c09e1e
woogi <kkjkkjcom1@gmail.com>
2021-05-29 16:44:32 +0900

    Added I am poor project. 에셋 이미지 첨부 및 오브젝트 추가 연습

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by 강태욱 on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
